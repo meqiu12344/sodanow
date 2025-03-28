2fa9abe9d01b3328745977013e96f6c3cbb1b54b
Mateusz <meqiu06@gmail.com>
2025-03-28 22:46:51 +0100

    Adding <Analiytics />

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ import Pricing from './components/pricing'
 import PopUpForm from './components/PopUpForm'
 import How_to_replace from './components/how_to_replace'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
 
 const page = () => {
   return (
     <div>
+      <Analytics />
       <Header />
       <PopUpForm />
       <Hero />
