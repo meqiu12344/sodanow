61962b131430044a38de7207c03a111cb1439821
Mateusz <meqiu06@gmail.com>
2025-04-10 18:43:11 +0200

    ceo update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,35 @@
 import './globals.css';
 
 export const metadata = {
-  title: 'SodaNow',
-  description: 'SodaNow - Wymiana butli CO2',
+  title: 'SodaNow | Wymiana Butli CO2 Szybko i Wygodnie - Wrocław i okolice',
+  description:
+    'SodaNow to szybka i wygodna wymiana butli CO2 do saturatorów na terenie Wrocławia i okolic. Zamów już dziś i ciesz się świeżą wodą sodową!',
+  keywords: [
+    'wymiana butli CO2',
+    'butle CO2 Wrocław',
+    'saturator CO2',
+    'gaz do saturatora',
+    'SodaStream wymiana',
+    'nabijanie butli CO2',
+  ],
+  robots: 'index, follow',
+  author: 'SodaNow Team',
+  viewport: 'width=device-width, initial-scale=1',
+  openGraph: {
+    title: 'SodaNow | Wymiana Butli CO2 Szybko i Wygodnie',
+    description:
+      'SodaNow to szybka i wygodna wymiana butli CO2 do saturatorów na terenie Wrocławia i okolic. Zamów już dziś!',
+    url: 'https://sodanow.vercel.app/',
+    siteName: 'SodaNow',
+    locale: 'pl_PL',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'SodaNow | Wymiana Butli CO2',
+    description:
+      'SodaNow to szybka i wygodna wymiana butli CO2 do saturatorów na terenie Wrocławia i okolic.',
+  },
 };
 
 export default function RootLayout({
